61b690cdcb02460f95e6aeb1faf00f9046e6eed1
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 13:31:19 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,10 @@
 pipeline {
     agent any
-
+    
+    environment {
+        AWS_REGION = 'ap-south-1'
+       /* AWS_CREDENTIALS = credentials('aws-credentials-id') */
+    }
     parameters {
         /* choice(name: 'REGION', choices: 'us-west-2a us-east-1', description: 'Select AWS Region') */
         string(name: 'grid-us-west-2a', defaultValue: '50', description: 'Weight for grid-us-west-2a')
@@ -13,6 +17,11 @@ pipeline {
         stage('Terraform Plan') {
             steps {
                 script {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AKIA4IOHQQ5W7EOLZ5TX', secretKeyVariable: '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']]) {
+                        
+                        // You can now use AWS credentials in this block
+                        sh "echo 'AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}'"
+                        sh "echo 'AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}'"
                     // Initialize and plan Terraform
                     sh "terraform init"
                     sh "terraform plan -var='grid-us-west-2a=${params.'grid-us-west-2a'}' -var='grid-us-east-1=${params.'grid-us-east-1'}' -var='tim-us-west-2a=${params.'tim-us-west-2a'}' -var='tim-us-east-1=${params.'tim-us-east-1'}'"
