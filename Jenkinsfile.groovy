44ac0405506dc66c8737c00603e6c234cfdfd16b
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 12:56:14 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     parameters {
-        choice(name: 'REGION', choices: 'us-west-2a us-east-1', description: 'Select AWS Region')
+        /* choice(name: 'REGION', choices: 'us-west-2a us-east-1', description: 'Select AWS Region') */
         string(name: 'grid-us-west-2a', defaultValue: '50', description: 'Weight for grid-us-west-2a')
         string(name: 'grid-us-east-1', defaultValue: '50', description: 'Weight for grid-us-east-1')
         string(name: 'tim-us-west-2a', defaultValue: '50', description: 'Weight for tim-us-west-2a')
@@ -15,7 +15,7 @@ pipeline {
                 script {
                     // Initialize and plan Terraform
                     sh "terraform init"
-                    sh "terraform plan -var='grid-us-west-2a=${params.grid-us-west-2a}' -var='grid-us-east-1=${params.grid-us-east-1}' -var='tim-us-west-2a=${params.tim-us-west-2a}' -var='tim-us-east-1=${params.tim-us-east-1}'"
+                    sh "terraform plan -var='grid-us-west-2a=${params.'grid-us-west-2a'}' -var='grid-us-east-1=${params.'grid-us-east-1'}' -var='tim-us-west-2a=${params.'tim-us-west-2a'}' -var='tim-us-east-1=${params.'tim-us-east-1'}'"
                 }
             }
         }
