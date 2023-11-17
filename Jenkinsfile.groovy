6b5c57afc1a8aa406ec461e3c1345c0b1fc71487
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 18:26:15 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,34 +1,32 @@
 pipeline {
     agent any
-    
-    environment {
-        AWS_REGION = 'ap-south-1'
-       /* AWS_CREDENTIALS = credentials('aws-credentials-id') */
-    }
+
     parameters {
         /* choice(name: 'REGION', choices: 'us-west-2a us-east-1', description: 'Select AWS Region') */
         string(name: 'grid-us-west-2a', defaultValue: '50', description: 'Weight for grid-us-west-2a')
         string(name: 'grid-us-east-1', defaultValue: '50', description: 'Weight for grid-us-east-1')
         string(name: 'tim-us-west-2a', defaultValue: '50', description: 'Weight for tim-us-west-2a')
         string(name: 'tim-us-east-1', defaultValue: '50', description: 'Weight for tim-us-east-1')
+        string(name: 'AWS_ACCESS_KEY_ID', description: 'AWS Access Key ID')
+        string(name: 'AWS_SECRET_ACCESS_KEY', description: 'AWS Secret Access Key')
     }
 
     stages {
         stage('Terraform Plan') {
             steps {
                 script {
-                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', secretKeyVariable: 'AWS_SECRET_KEY']]) {
+                    withCredentials([string(credentialsId: '7469a313-8421-4018-85f2-5fd390bd6e80', variable: 'AWS_ACCESS_KEY_ID'), string(credentialsId: '7469a313-8421-4018-85f2-5fd390bd6e80', variable: 'AWS_SECRET_ACCESS_KEY')]) {
                         
                         // You can now use AWS credentials in this block
                         sh "echo 'AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}'"
-                        sh "echo 'AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}'"
+                        sh "echo 'AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}'"
                     // Initialize and plan Terraform
                     sh "terraform init"
                     sh "terraform plan -var='grid-us-west-2a=${params.'grid-us-west-2a'}' -var='grid-us-east-1=${params.'grid-us-east-1'}' -var='tim-us-west-2a=${params.'tim-us-west-2a'}' -var='tim-us-east-1=${params.'tim-us-east-1'}'"
+                    }    
                 }
+          
             }
         }
     }
-}
-    
 }
