26ae6205c265f890aec12f0c6ca496309bf2cb59
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 18:55:52 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,26 +6,13 @@ pipeline {
         string(name: 'grid-us-west-2a', defaultValue: '50', description: 'Weight for grid-us-west-2a')
         string(name: 'grid-us-east-1', defaultValue: '50', description: 'Weight for grid-us-east-1')
         string(name: 'tim-us-west-2a', defaultValue: '50', description: 'Weight for tim-us-west-2a')
-        string(name: 'tim-us-east-1', defaultValue: '50', description: 'Weight for tim-us-east-1')
-        string(name: 'AWS_CREDENTIALS_ID', description: 'AWS Credentials ID')
-     
+        string(name: 'tim-us-east-1', defaultValue: '50', description: 'Weight for tim-us-east-1')     
     }
 
     stages {
         stage('Terraform Plan') {
             steps {
                 script {
-                    def awsCredentials = credentials(params.'7469a313-8421-4018-85f2-5fd390bd6e80')
-                    withCredentials([[
-                        $class: 'AmazonWebServicesCredentialsBinding',
-                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                        credentialsId: awsCredentials.id,
-                        secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-                    ]]) {
-                        
-                        // You can now use AWS credentials in this block
-                        sh "echo 'AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}'"
-                        sh "echo 'AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}'"
                     // Initialize and plan Terraform
                     sh "terraform init"
                     sh "terraform plan -var='grid-us-west-2a=${params.'grid-us-west-2a'}' -var='grid-us-east-1=${params.'grid-us-east-1'}' -var='tim-us-west-2a=${params.'tim-us-west-2a'}' -var='tim-us-east-1=${params.'tim-us-east-1'}'"
@@ -35,4 +22,3 @@ pipeline {
             }
         }
     }
-}
