ea3a18a47cbfd975d09d7ff0f0e71a140b1c4eb5
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 13:11:50 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "grid-us-west-2a" {
+    description = "service1 weighted record for grid-us-west-2a"
+    default = 50
+   }
+variable "grid-us-east-1" {
+    description = "service1 weighted record grid-us-east-1"
+    default = 50
+   }
+variable "tim-us-west-2a" {
+    description = "service1 weighted record tim-us-west-2a"
+    default = 50
+   }
+variable "tim-us-east-1" {
+    description = "service1 weighted record tim-us-east-1"
+    default = 50
+   }
