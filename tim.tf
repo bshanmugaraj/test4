f6f82bf95cea2da7aa87dba5bd8caa5f2a2f8db3
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-17 12:01:00 +0530

    Update and rename service2.tf to tim.tf

--- tim.tf
+++ tim.tf
@@ -1,27 +1,27 @@
-resource "aws_route53_record" "www-dev2" {
+resource "aws_route53_record" "tim-us-west-2a" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www"
   type    = "CNAME"
   ttl     = 5
 
   weighted_routing_policy {
-    weight = var.service2_www-dev2
+    weight = var.tim-us-west-2a
   }
 
-  set_identifier = "prod"
+  set_identifier = "tim-us-west-2a"
   records        = ["prod.example.com"]
 }
 
-resource "aws_route53_record" "www-live2" {
+resource "aws_route53_record" "tim-us-east-1" {
   zone_id = aws_route53_zone.primary.zone_id
   name    = "www"
   type    = "CNAME"
   ttl     = 5
 
   weighted_routing_policy {
-    weight = var.service2_www-live2
+    weight = var.tim-us-east-1
   }
 
-  set_identifier = "nonprod"
+  set_identifier = "tim-us-east-1"
   records        = ["nonprod.example.com"]
 }
