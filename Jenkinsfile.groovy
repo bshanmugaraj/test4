4d2448437a9fdad25ba0abd575f68c2b337ce726
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-18 19:27:42 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,13 @@
 node {
-    stage('Checkout') {
-        // Assuming your Terraform configurations are in a directory named 'terraform'
-        checkout scm
-        withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY'),
-        string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')])
-    }
+    stage "Checkout" 
+    projectRootDirectory = pwd()
+    println ("project root: " + projectRootDirectory)
+    println ("cleaning project root: " + projectRootDirectory)
+    sh "sudo rm -rf ${projectRootDirectory} || true ; mkdir -p ${projectRootDirectory}"
+    checkout scm
+    withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY'),
+                     string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')]) 
+    
 
     stage('Terraform Plan') {
         // List of Terraform files to Plan
