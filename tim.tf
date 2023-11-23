f97da2190452805a7fdcd7c31a00862677a0dede
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-23 16:50:43 +0530

    Update tim.tf

--- tim.tf
+++ tim.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "tim-us-west-2a" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "www"
+  name    = "www-rr"
   type    = "CNAME"
   ttl     = 5
 
   weighted_routing_policy {
-    weight = var.tim-us-west-2a
+    weight = 0
   }
 
   set_identifier = "tim-us-west-2a"
@@ -14,12 +14,12 @@ resource "aws_route53_record" "tim-us-west-2a" {
 
 resource "aws_route53_record" "tim-us-east-1" {
   zone_id = aws_route53_zone.primary.zone_id
-  name    = "www"
+  name    = "www-rr"
   type    = "CNAME"
   ttl     = 5
 
   weighted_routing_policy {
-    weight = var.tim-us-east-1
+    weight = 100
   }
 
   set_identifier = "tim-us-east-1"
