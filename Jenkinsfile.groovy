c67f4b242359519692b5c22760817e8a02c32018
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-11-23 17:34:01 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,26 +1,43 @@
 node {
-    stage "Checkout" 
-    projectRootDirectory = pwd()
+    stage "Checkout"
+    projectRootDirectory = pwd() 
     println ("project root: " + projectRootDirectory)
     println ("cleaning project root: " + projectRootDirectory)
     sh "sudo rm -rf ${projectRootDirectory} || true ; mkdir -p ${projectRootDirectory}"
     checkout scm
     withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY'),
                      string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')]) {
-    
+    parameters {
+        string(name: 'TARGET_NAME', defaultValue: 'www-rr', description: 'Target Name of the CNAME')
+        string(name: 'TARGET_RECORD', defaultValue: 'prod.example.com', description: 'Target Record')
+        string(name: 'NEW_WEIGHT', defaultValue: '0', description: 'New Weight for record change')
+    }
 
     stage('Terraform Plan') {
-        // List of Terraform files to Plan
-        def terraformFiles = ['tim.tf', 'grid.tf']
 
-        // Loop through each Terraform file and apply
-        terraformFiles.each { file ->
-            echo "Applying Terraform configuration in $file"
-            
-            // Run Terraform init and apply for each file
-            sh "terraform init -input=false -backend-config=backend.config -var-file=variables.tfvars"
-            sh "terraform plan -var 'file=$file'"
-         }
-      }
-   }
+                    // Sample data representing DNS records
+                    def dnsRecords = [
+                        [name: 'www-rr', records: ['prod.example.com'], weight: 0],
+                        [name: 'other-rr', records: ['test.example.com'], weight: 50],
+                    ]
+
+                    // Find the record matching the criteria
+                    def targetRecord = dnsRecords.find { it.name == params.TARGET_NAME && it.records.contains(params.TARGET_RECORD) }
+
+                    // Check if the target record was found
+                    if (targetRecord) {
+                        // Change the weight to the user-provided value
+                        targetRecord.weight = Integer.parseInt(params.NEW_WEIGHT)
+
+                        // Print the updated DNS records
+                        echo "Updated DNS Records:"
+                        dnsRecords.each { record ->
+                            echo "${record.name}: ${record.records} - Weight: ${record.weight}"
+                        }
+                    } else {
+                        // If the record is not found, print a message
+                        echo "Record with name='${params.TARGET_NAME}' and records='${params.TARGET_RECORD}' not found."
+                    }
+        }
+    }
 }
