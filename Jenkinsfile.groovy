644cefb4275b8e13b553fcad8572011ef8eea9d4
narezchand <nareshbabubc@gmail.com>
2023-11-27 15:23:16 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,6 +12,44 @@ node {
     stage('Update File') {
         println "${env.SERVICE}"
         println "$SERVICE"
+        def updateContent(String filePath, String recordName, String weightValue){
+        def contents = new File( filePath ).text.readLines()
+        def newContents = new ArrayList();
+        for(int i=0;i<contents.size();i++){
+            newContents.add(i, contents.get(i));
+            if (contents.get(i).contains(recordName)){
+                for(int j = i; j>=0;j--){
+                    if(contents.get(j).contains("weight")){
+                        String str = contents.get(j);
+                        String[] weight = extractInts(str);
+                        str = contents.get(j).replace(weight[0], weightValue);
+                        newContents.set(j, str);
+                        break;
+                    }
+                }
+            }
+        
+        }
+
+            File f = new File(filePath)
+            PrintWriter writer = new PrintWriter(f)
+            newContents.each { id -> writer.println(id) }
+            writer.close()
+        }
+
+
+        def extractInts(String input){
+        input.findAll( /\d+/ )*.toInteger()
+    }
+
+        if ("${dev.example.com}" != "" {
+            updateContent("${SERVICE}.tf","dev.example.com","${dev.example.com}");
+        }
+        else if ("${prod.example.com}" != "" {
+            updateContent("${SERVICE}.tf","prod.example.com","${prod.example.com}");
+        }
+        else
+             println "No weights was assigned";
     }
 
 
