2839a95892d6884ab46bbd66e286520d62717c29
narezchand <nareshbabubc@gmail.com>
2023-11-27 14:31:31 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,9 +6,9 @@ node {
     sh "sudo rm -rf ${projectRootDirectory} || true ; mkdir -p ${projectRootDirectory}"
     checkout scm
     withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY'),
-                     string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')])
+                     string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')]){
 
-    }
+    
     stage('Update File') {
         
     }
@@ -27,5 +27,6 @@ node {
     stage('Commit'){
 
     }
-       
+    }    
+}
 }
