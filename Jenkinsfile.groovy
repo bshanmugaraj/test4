b2c0731985b1762d33a67d452526c865c5da7042
bshanmugaraj <53233353+bshanmugaraj@users.noreply.github.com>
2023-12-01 15:27:56 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+node {
+    stage ("Checkout"){
+    projectRootDirectory = pwd() 
+    println ("project root: " + projectRootDirectory)
+    println ("cleaning project root: " + projectRootDirectory)
+    sh "sudo rm -rf ${projectRootDirectory} || true ; mkdir -p ${projectRootDirectory}"
+    checkout scm
+    withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY'),
+                     string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_KEY')]){
+
+
+
+    stage('Update File') {
+    echp "update file stage"
+    }
+      
+    stage('Plan') {
+      sh 'terraform plan'
+    }
+      
+    stage('Approval') {
+      // Manual approval step
+      input 'Proceed with Terraform apply?'
+    }
+        
+
+    stage('Terraform Apply') {
+      // Execute Terraform apply
+      sh 'set +e; terraform apply; echo \$? > status.apply'
+      def applyExitCode = readFile('status.apply').trim()
+      if (applyExitCode == "0") {
+         slackSend channel: "${env.SLACK_CHANNEL_NAME}", color: 'good', message: "@here Changes Applied ${env.JOB_NAME} - ${env.BUILD_NUMBER} (<${env.BUILD_URL}console|Open>)"
+      } else 
+         slackSend channel: "${env.SLACK_CHANNEL_NAME}", color: 'danger', message: "@here Apply Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER} (<${env.BUILD_URL}console|Open>)"
+         currentBuild.result = 'FAILURE'
+    }
+    }
+    }
+}
